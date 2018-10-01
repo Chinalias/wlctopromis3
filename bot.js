c373ac1c3eb29d51a025ad751ffccb98133c0916
bakutkisa241 <35742714+bakutkisa241@users.noreply.github.com>
2018-10-02 01:44:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== '496454077069393961') return;
+  if (message.author.id !== '496463461316952094') return;
 if (message.content.startsWith(PrEfix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
