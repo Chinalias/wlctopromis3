210303313396148cd381c9ad4489a1d140da2023
laimamssd <35742714+laimamssd@users.noreply.github.com>
2018-10-26 16:18:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,9 @@ client.on('ready', () => {
 });
  
 client.on('guildMemberAdd' ,member => {
-  if(member.guild.id !== '449705743000666124') return;
+  if(member.guild.id !== '497101650037702669') return;
   setTimeout(function(){
-  member.guild.channels.find(r => r.id === '489175220377354270').send('**Youre In  HollyWood , Welcome ..**');
+  member.guild.channels.find(r => r.id === '498405076805615626').send('**Wélcome To Pròmise Śerver .𖤐  **');
 },3000);
 });
 var PrEfix = "$";
@@ -17,7 +17,7 @@ client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== '496463461316952094') return;
+  if (message.author.id !== '486566948797415456') return;
 if (message.content.startsWith(PrEfix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
@@ -28,7 +28,7 @@ client.user.setActivity(argresult, {type:'LISTENING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else 
 if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/amira");
+  client.user.setGame(argresult, "https://www.twitch.tv/Rose");
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 }
 
